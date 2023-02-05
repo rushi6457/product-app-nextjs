eccccf6c8e056e2e8f3371cdf57079dfec41fc81
rushi6457 <rushiypatil@gmail.com>
2023-02-05 12:01:12 +0530

    pending

--- server/src/routes/dist/productRoute.dev.js
+++ server/src/routes/dist/productRoute.dev.js
@@ -3,3 +3,15 @@
 var express = require('express');
 
 var ProductModel = require('../models/productModel');
+
+var addProducts = function addProducts(req, res) {};
+
+var deleteProduct = function deleteProduct(req, res) {};
+
+var updateProduct = function updateProduct(req, res) {};
+
+module.exports = {
+  addProducts: addProducts,
+  deleteProduct: deleteProduct,
+  updateProduct: updateProduct
+};
