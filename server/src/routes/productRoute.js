baa90ace6397ecfda4c0795871c1a393bac37edb
rushi6457 <rushiypatil@gmail.com>
2023-02-05 11:59:44 +0530

    pending

--- server/src/routes/productRoute.js
+++ server/src/routes/productRoute.js
@@ -1,5 +1,22 @@
 const express = require('express');
 const ProductModel = require('../models/productModel');
 
+const addProducts = (req,res) =>{
+
+
+}
+
+const deleteProduct = (req,res) =>{
+
+}
+const updateProduct = (req,res) =>{
+
+}
+
+module.exports = {
+    addProducts,
+    deleteProduct,
+    updateProduct
+}
 
 
